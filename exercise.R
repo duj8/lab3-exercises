b849664aec0bd64242ac5887d2075aabaf1bf3fd
Justin Du <duj8@uw.edu>
2019-04-23 20:41:28 -0700

    save progress

--- exercise.R
+++ exercise.R
@@ -1,13 +1,23 @@
 # Download the iris dataset
-data('iris')
+data(iris)
 
 # Check to see if the downloaded data set is a data frame
 
+is.data.frame(iris)
+
 # Select all values of data frame for Sepal.length
+
+iris$Sepal.Length
+iris[, 'Sepal.Length']
 
 # Select only rows of Virginica flowers
 
+virginica <- iris[iris$Species == 'virginica', ]
+
+
 # Select only the sepal legnth of the Virginica flowers
+
+virginica_sepal_length <- virginica$Sepal.Length
 
 # Select rows where Petal.Length > 4.0
 
